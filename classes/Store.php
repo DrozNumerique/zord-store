cef861087edfc2dbe97e7cd07b3bbae6a7081387
EA <arrive@free.fr>
2020-01-22 19:15:53 +0100

    enhancements

--- classes/Store.php
+++ classes/Store.php
@@ -2,69 +2,43 @@
 
 class Store {
     
-	public static function data($isbn, $type = 'path', $format = 'path') {
-	    $base = DATA_FOLDER.'zord'.DS.$isbn;
-	    $path = null;
+	public static function data($ean, $path = null, $format = 'path') {
+	    $base = DATA_FOLDER.'zord'.DS.$ean;
+	    $path = $base.(isset($path) ? DS.$path : '');
 	    $data = null;
-	    switch ($type) {
-	        case 'path': {
-	            $path = $base.DS;
-	            break;
-	        }
-	        case 'temp': {
-	            $path = $base.'.tmp'.DS;
-	            break;
-	        }
-	        case 'meta': {
-	            $path = $base.DS.'metadata.json';
-	            break;
-	        }
-	        default: {
-	            $file = Zord::value('store', $type);
-	            if (isset($file)) {
-	                $path = $base.DS.$file;
-	            } else {
-	                $ext = Zord::value('store', 'default');
-	                $path = $base.DS.$type.'.'.$ext;
-	            }
-	            break;
-	        }
-	    }
-	    if (isset($path)) {
-    	    switch ($format) {
-    	        case 'path': {
-    	            $data = $path;
-    	            break;
-    	        }
-    	        case 'content': {
-    	            $data = file_get_contents($path);
-    	            break;
-    	        }
-    	        case 'object': {
-    	            $data = json_decode(file_get_contents($path));
-    	            break;
-    	        }
-    	        case 'array': {
-    	            $data = Zord::arrayFromJSONFile($path);
-    	            break;
-    	        }
-    	        case 'document': {
-    	            $data = new DOMDocument();
-    	            $data->load($path);
-    	            break;
-    	        }
-    	        case 'xml': {
-    	            $data = simplexml_load_string(file_get_contents($path), "SimpleXMLElement", LIBXML_NOCDATA);
-    	            break;
-    	        }
-    	    }
+   	    switch ($format) {
+   	        case 'path': {
+   	            $data = $path;
+   	            break;
+   	        }
+   	        case 'content': {
+   	            $data = file_get_contents($path);
+   	            break;
+   	        }
+   	        case 'object': {
+   	            $data = json_decode(file_get_contents($path));
+   	            break;
+   	        }
+   	        case 'array': {
+   	            $data = Zord::arrayFromJSONFile($path);
+   	            break;
+   	        }
+   	        case 'document': {
+   	            $data = new DOMDocument();
+   	            $data->load($path);
+   	            break;
+   	        }
+   	        case 'xml': {
+   	            $data = simplexml_load_string(file_get_contents($path), "SimpleXMLElement", LIBXML_NOCDATA);
+   	            break;
+   	        }
 	    }
 	    return $data;
 	}
 	
-	public static function media($isbn, $names = null, $types = ['jpg','png']) {
+	public static function media($ean, $names = null, $types = ['jpg','png']) {
 	    if (!isset($names)) {
-	        return DATA_FOLDER.'medias'.DS.$isbn.DS;
+	        return DATA_FOLDER.'medias'.DS.$ean.DS;
 	    }
 	    if (!is_array($names)) {
 	        $names = [$names];
@@ -74,12 +48,22 @@ class Store {
 	    }
 	    foreach ($names as $name) {
 	        foreach ($types as $ext) {
-	            $media = 'medias'.DS.$isbn.DS.$name.'.'.$ext;
+	            $media = 'medias'.DS.$ean.DS.$name.'.'.$ext;
 	            if (file_exists(DATA_FOLDER.$media)) {
 	                return $media;
 	            }
 	        }
 	    }
 	    return false;
+	}
+	
+	public static function isbn($ean) {
+	    $tokens = [];
+	    $pos = 0;
+	    foreach ([3,1,3,5,1] as $length) {
+	        $tokens[] = substr($ean, $pos, $length);
+	        $pos += $length;
+	    }
+	    return implode('-', $tokens);
 	}
 }
